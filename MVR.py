364994da4df977600ef0ba078bb930d74506a164
uday1874533 <119723458+uday1874533@users.noreply.github.com>
2023-07-13 09:57:19 +0530

    Add files via upload

---
+++ MVR.py
@@ -0,0 +1,14 @@
+# 5 45 SELF VENKY pass
+# cook your dish here
+
+
+a,b,x,y=map(int,input().split()) 
+c=2*a+b 
+d=2*x+y
+if(c==d): 
+    print("equal") 
+elif(c>d): 
+    print("messi") 
+else: 
+    print("ronaldo")
+    
