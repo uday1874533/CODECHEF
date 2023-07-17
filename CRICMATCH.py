4c3dfd5dc050fdc48f257b452d2137da92a97787
uday1874533 <119723458+uday1874533@users.noreply.github.com>
2023-07-17 09:35:46 +0530

    Add files via upload

---
+++ CRICMATCH.py
@@ -0,0 +1,9 @@
+# cook your dish here
+# LANDED M2 PREP ,HALLTICKET HANGONNN
+
+for _ in range(int(input())):
+    n, m = map(int, input().split())
+    if m*36 >= n:
+        print("YES")
+    else:
+        print("NO")
