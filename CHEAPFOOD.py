b808372a37b73a669a688686d1fb05e840fc8999
uday1874533 <119723458+uday1874533@users.noreply.github.com>
2023-07-21 09:28:39 +0530

    Add files via upload

---
+++ CHEAPFOOD.py
@@ -0,0 +1,13 @@
+# PROJECT K 
+#KALKI 2898
+# cook your dish here
+t=int(input())
+for i in range(t):
+    x=int(input())
+    b=int(x*0.1)
+    z=100
+    if(b>z):
+        print(b)
+    else:
+        print(z)
+
