4043b0d9517dc79d649f5d8282579f4cbe999b34
uday1874533 <119723458+uday1874533@users.noreply.github.com>
2023-07-24 16:37:32 +0530

    Add files via upload

---
+++ WATERCOOLER1.py
@@ -0,0 +1,14 @@
+# cook your dish here 
+# osdmnah co pachd
+#a i w 
+
+# 24
+n=int(input())
+for i in range(n):
+    a,b,c=map(int,input().split())
+    d=a*c
+    if(d<b):
+        print("YES")
+    else:
+        print("NO")
+# done siddu
