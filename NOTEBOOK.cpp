5ce729be466bc522c437dbcc885ce3ba2495d1ba
uday1874533 <119723458+uday1874533@users.noreply.github.com>
2023-07-28 21:38:44 +0530

    Add files via upload

---
+++ NOTEBOOK.cpp
@@ -0,0 +1,18 @@
+// VEERU //BRO JULY 28TH
+
+// WASHING MACHINE
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    double N;
+	    cin>>N;
+	    cout<<(N*1000)/100<<endl;
+	}
+	return 0;
+}
