79e785763e496c53f13107abb514de6d2c481a3a
uday1874533 <119723458+uday1874533@users.noreply.github.com>
2023-07-29 10:16:52 +0530

    Add files via upload

---
+++ SUGARCANE.cpp
@@ -0,0 +1,16 @@
+// hoina hoina hoina gangleader
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t,n;
+    std::cin >> t;
+    while(t--){
+        std::cin >> n;
+        // std::cout << n*(50-35) << std::endl;
+        std::cout << n*15 << std::endl;
+    }
+	return 0;
+}
