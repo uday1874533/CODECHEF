1b15c1355326775c487680c1c24431acbe2b7217
uday1874533 <119723458+uday1874533@users.noreply.github.com>
2023-08-07 10:06:31 +0530

    Add files via upload

---
+++ JENGA.py
@@ -0,0 +1,12 @@
+# 1ST DAY 2 COLLEGE AND OS class
+#  OS 
+# CLASS OF VEERABABU CLASS OS AND AMMERPETTA
+# cook your dish here
+t = int(input())
+for i in range(t):
+    n,x = map(int,input().split())
+    if x%n==0:
+        print("yes")
+    else:
+        print("no")
+
