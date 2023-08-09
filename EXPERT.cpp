d215955f25334a91008b748b7928139c59443f49
uday1874533 <119723458+uday1874533@users.noreply.github.com>
2023-08-09 14:04:57 +0530

    Add files via upload

---
+++ EXPERT.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int t; cin>>t;
+	while(t--)
+	{
+		float x,y; cin>>x>>y;
+		float r = (y/x)*100;
+	
+		if(r>=50)
+		cout<<"YES\n";
+		else
+		cout<<"NO\n";
+	}
+}
+
+// BRO JULY 28TH
