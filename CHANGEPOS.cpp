39a08759f64a56c1adbc7e7a6195ccd800bf3dc3
uday1874533 <119723458+uday1874533@users.noreply.github.com>
2023-08-24 08:02:56 +0530

    Add files via upload

---
+++ CHANGEPOS.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+// LINKEDIN POST DONE
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a,b,c,d;
+	    cin>>a>>b>>c>>d;
+	    
+	    if(a!=c && b!=d)
+	        cout<<1<<endl;
+	        
+	    else 
+	        cout<<2<<endl;
+	}
+	return 0;
+}
