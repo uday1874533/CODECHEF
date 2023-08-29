23cc6eccf9cf52eec97ec79d07c451db1e4c93e1
uday1874533 <119723458+uday1874533@users.noreply.github.com>
2023-08-29 10:00:47 +0530

    Add files via upload

---
+++ BLACKJACK.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b;
+	    cin>>a>>b;
+	    if(a+b<=10)
+	    cout<<-1<<endl;
+	    else
+	    cout<<21-(a+b)<<endl;
+	    
+	}
+	return 0;
+}
