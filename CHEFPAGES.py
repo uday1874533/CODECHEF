e4a6eb55c1ac2d162b07c5f2ffed4a56daf74a7a
uday1874533 <119723458+uday1874533@users.noreply.github.com>
2023-09-18 13:08:37 +0530

    Add files via upload

---
+++ CHEFPAGES.py
@@ -0,0 +1,13 @@
+# PEDAMMA BIRTHDAY SEP 18
+# VINAKA CHAVITHI 
+# SZZZ
+
+# cook your dish here
+a,b = map(int,input().split())
+if a==0:
+    print('https://www.codechef.com/practice')
+elif b==0:
+    print('https://www.codechef.com/contests')
+else:
+    print('https://discuss.codechef.com')
+
