bcaaa3f9784dc9bcc5cdb964a33f2463f373b217
uday1874533 <119723458+uday1874533@users.noreply.github.com>
2023-09-24 10:02:25 +0530

    Add files via upload

---
+++ SELFDEF.py
@@ -0,0 +1,7 @@
+# DAY 1 bunny mot
+
+# cook your dish here
+for _ in range(int(input())):
+    n = int(input())
+    x = list(filter(lambda x: 10 <= x <=60, list(map(int, input().split(' ')))))
+    print(len(x))
