3ae7cf1998199a1fef8fcbff27c559d199b92cca
uday1874533 <119723458+uday1874533@users.noreply.github.com>
2023-10-02 10:04:54 +0530

    Add files via upload

---
+++ JGAMES.py
@@ -0,0 +1,12 @@
+# cook your dish here
+# JUST DOLO 
+t=int(input())
+while t:
+    x,y=map(int,input().split())
+    if (x+y)%2==0:
+        print("Janmansh")
+    else:
+        print("Jay")
+    t-=1 
+    
+        
