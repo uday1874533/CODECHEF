cb8604643d909769320334fd569226474e37922b
uday1874533 <119723458+uday1874533@users.noreply.github.com>
2023-10-05 18:08:29 +0530

    Add files via upload

---
+++ FLOW008.py
@@ -0,0 +1,11 @@
+# RAMAGUNDAMM MNY 
+# ENG VS NZ 1ST WORLD CUP MATCH
+# 282/9
+
+# cook your dish here
+for i in range (int(input())):
+    n=int(input())
+    if(n<10):
+        print("Thanks for helping Chef!")
+    else:
+        print("-1")
