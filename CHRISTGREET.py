a4ed64f2f294a31e5ab525015b3558ce8d4f1855
uday1874533 <119723458+uday1874533@users.noreply.github.com>
2023-12-27 20:52:47 +0530

    Add files via upload

---
+++ CHRISTGREET.py
@@ -0,0 +1,15 @@
+# cook your dish here
+
+# KL RAHUL 101 TOUGH FIGHT
+
+
+
+# Get the input
+day_in_december = int(input())
+
+# Check if the given day is Christmas (25th December)
+if day_in_december == 25:
+    print("CHRISTMAS")
+else:
+    print("ORDINARY")
+
