61d4829bec61ecabb2a11551cf88599439bb3d9a
uday1874533 <119723458+uday1874533@users.noreply.github.com>
2024-01-10 10:03:40 +0530

    Add files via upload

---
+++ THALA7.py
@@ -0,0 +1,13 @@
+# cook your dish here
+# 7 
+# BOLE JO KOYAL BAGO MEIN .......................
+
+
+def print_string_based_on_number(n):
+    if n == 7:
+        print("THALA")
+    else:
+        print("SADGE")
+n = int(input().strip())
+print_string_based_on_number(n)
+
