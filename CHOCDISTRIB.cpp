2924d87e195f875cb609a02d7cc712c428a2dd0d
uday1874533 <119723458+uday1874533@users.noreply.github.com>
2024-01-14 10:29:32 +0530

    Add files via upload

---
+++ CHOCDISTRIB.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int test_cases;
+	cin >> test_cases;
+	while(test_cases--){
+	    // SOUTH AFRICA ON FIRE
+	    //BUT INDIA COME BACK
+	    int number;
+	    cin >> number;
+	    if(number % 2==0){
+	        cout << number / 2 << ' ' << number << endl;
+	    } else {
+	        cout << number / 2+1 << ' ' << number << endl;
+	        // krerg
+	        // etguwhetoug
+	        
+	    }
+	}
+	return 0;
+
+}
