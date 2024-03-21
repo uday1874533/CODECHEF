1622d549eb07bce133c2a33ec8a111626a94cc8f
uday1874533 <119723458+uday1874533@users.noreply.github.com>
2024-03-21 11:19:56 +0530

    Add files via upload

---
+++ PERDIS.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t > 0) {
+        int n;
+        cin >> n;
+
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+            arr[i]--; // decrementing the input by 1
+        }
+
+        int start = 0;
+        int ans = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] == i) {
+                int temp = arr[i + 1];
+                arr[i + 1] = arr[i];
+                arr[i] = temp;
+                ans++;
+            }
+        }
+        if (arr[n - 1] == n - 1)
+            ans++;
+
+        cout << ans << endl;
+        t--;
+    }
+
+    return 0;
+}
