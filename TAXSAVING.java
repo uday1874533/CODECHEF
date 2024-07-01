0ae7d50e0adbbc78ae9b62619032ad28148c97ef
V UDAY SAI SIDDARDHA <119723458+uday1874533@users.noreply.github.com>
2024-07-01 10:10:46 +0530

    Add files via upload

---
+++ TAXSAVING.java
@@ -0,0 +1,20 @@
+// IND VS SA WC FINALS 2024 
+// 29TH JUNE 2024
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc= new Scanner (System.in);
+		int t= sc.nextInt();
+		while(t-->0){
+		    int x= sc.nextInt();
+		    int y= sc.nextInt();
+		    System.out.println(x-y);
+		}
+	}
+}
