17ec9df1465797fdecee4a17f0e6469b35bdffd2
V UDAY SAI SIDDARDHA <119723458+uday1874533@users.noreply.github.com>
2024-09-25 20:12:10 +0530

    Add files via upload

---
+++ AICOM.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef {
+    public static void main (String[] args) throws java.lang.Exception {
+        Scanner mouni = new Scanner(System.in);
+        int siddhu = mouni.nextInt();
+        
+        if (siddhu <= 60) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+        
+        mouni.close();
+    }
+}
