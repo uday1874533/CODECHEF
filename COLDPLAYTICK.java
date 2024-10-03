7fb30abdf7c80fbd7e619346007c37d8c50d0e1a
uday_siddardha_369 <119723458+uday1874533@users.noreply.github.com>
2024-10-03 09:43:29 +0530

    Add files via upload

---
+++ COLDPLAYTICK.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef {
+    public static void main (String[] args) throws java.lang.Exception {
+        Scanner siddhu = new Scanner(System.in);
+        int mouni = siddhu.nextInt();
+        System.out.println((mouni + 1) * 5000);
+    }
+}
