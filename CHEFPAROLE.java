51103db0919329c765dfa44284bd8ed8ba4ebb8e
V UDAY SAI SIDDARDHA <119723458+uday1874533@users.noreply.github.com>
2024-10-10 17:29:49 +0530

    Add files via upload

---
+++ CHEFPAROLE.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner SIDDHU = new Scanner(System.in);
+        
+        int DAD = SIDDHU.nextInt();
+        
+        if (DAD >= 7) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
