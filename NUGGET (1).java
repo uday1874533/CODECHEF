c94991684c2418c3d3919059ce7cbf4d8b7dffe4
V UDAY SAI SIDDARDHA <119723458+uday1874533@users.noreply.github.com>
2024-11-20 22:00:39 +0530

    Add files via upload

---
+++ NUGGET (1).java
@@ -0,0 +1,15 @@
+import java.util.*; 
+import java.lang.*; 
+import java.io.*;  
+
+class Codechef {  
+    public static void main (String[] args) throws java.lang.Exception {  
+        Scanner uday = new Scanner(System.in); 
+        int lakshmi = uday.nextInt(); 
+        int prasanna = uday.nextInt(); 
+        
+        int totalMoney = lakshmi * 5000 + prasanna * 9800; 
+        
+        System.out.println(totalMoney);
+    }  
+}
