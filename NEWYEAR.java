4ae8028572427b465a98ff56ec25e7cba9c427d9
V UDAY SAI SIDDARDHA <119723458+uday1874533@users.noreply.github.com>
2025-01-01 20:21:39 +0530

    Add files via upload

---
+++ NEWYEAR.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner MOUNIKA_LAKSHMI_PRASANNA = new Scanner(System.in);
+        int UDAY = MOUNIKA_LAKSHMI_PRASANNA.nextInt();
+        int hoursLeft = 24 - UDAY;
+        System.out.println(hoursLeft);
+        MOUNIKA_LAKSHMI_PRASANNA.close();
+    }
+}
