1a2f2a0dca88569411100c3b4cedaf4c867c05a9
V UDAY SAI SIDDARDHA <119723458+uday1874533@users.noreply.github.com>
2025-01-23 09:40:21 +0530

    Add files via upload

---
+++ HJJ.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class BenchPress {
+    public static void main(String[] args) {
+        Scanner SIDDHU = new Scanner(System.in);
+        int MOON = SIDDHU.nextInt();
+        int thirdSetWeight = MOON + 20;
+        System.out.println(thirdSetWeight);
+        SIDDHU.close();
+    }
+}
