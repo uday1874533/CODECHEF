4a528c10c807122ced444a436843c9691f61181e
V UDAY SAI SIDDARDHA <119723458+uday1874533@users.noreply.github.com>
2025-04-09 10:53:44 +0530

    Add files via upload

---
+++ THALA7 (1).cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+// THALA FOR A REASON
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	if(n==7) cout<<"THALA";
+	else cout<<"SADGE";
+	return 0;
+}
